52486256155390952bbc713a9add2e1e419a3bd0
AndreLeB <eal4294@umoncton.ca>
2022-04-30 16:30:29 -0300

    Add portfolio

--- js/freelancer.js
+++ js/freelancer.js
@@ -32,13 +32,35 @@
 
     // Floating label headings for the contact form
     $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $("body").on("input propertychange", ".floating-label-form-group", 
+        function(e) {
             $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
+        }).on("focus", ".floating-label-form-group", 
+        function() {
             $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
+        }).on("blur", ".floating-label-form-group", 
+        function() {
             $(this).removeClass("floating-label-form-group-with-focus");
         });
+    });
+
+    // Translation
+    $('[lang="en"]').show(); // show just English text (you can change it)
+    $('.lang-switch').change(function ()  { // put onchange event when user select option from select
+    var lang = $(this).val(); // decide which language to display using switch case. The rest is obvious (i think)
+    switch (lang) {
+        case 'en':
+            $('[lang]').hide();
+            $('[lang="en"]').show();
+        break;
+        case 'fr':
+            $('[lang]').hide();
+            $('[lang="fr"]').show();
+        break;
+        default:
+            $('[lang]').hide();
+            $('[lang="en"]').show();
+        }
     });
 
 })(jQuery); // End of use strict
