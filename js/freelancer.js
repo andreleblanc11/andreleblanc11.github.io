ba055e48d674c2289f6bdf2cae5164bc6e144037
AndreLeB <eal4294@umoncton.ca>
2022-05-01 15:49:29 -0300

    Add about and books template

--- js/freelancer.js
+++ js/freelancer.js
@@ -44,23 +44,4 @@
         });
     });
 
-    // Translation
-    $('[lang="en"]').show(); // show just English text (you can change it)
-    $('.lang-switch').change(function ()  { // put onchange event when user select option from select
-    var lang = $(this).val(); // decide which language to display using switch case. The rest is obvious (i think)
-    switch (lang) {
-        case 'en':
-            $('[lang]').hide();
-            $('[lang="en"]').show();
-        break;
-        case 'fr':
-            $('[lang]').hide();
-            $('[lang="fr"]').show();
-        break;
-        default:
-            $('[lang]').hide();
-            $('[lang="en"]').show();
-        }
-    });
-
 })(jQuery); // End of use strict
